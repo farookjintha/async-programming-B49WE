1dc6f8bc2c62124129a3651f1251e48e7d8680be
Farook <farook.jintha@ace-alternatives.com>
2023-08-26 12:06:05 +0530

    added promise.all()

--- day3-asyncAwait/promiseChaining.js
+++ day3-asyncAwait/promiseChaining.js
@@ -11,18 +11,58 @@
 //     console.log("ERROR: ", error);
 //   });
 
-async function getTodo() {
+// async function getTodo() {
+//   try {
+//     const response = await fetch(
+//       "https://jsonplaceholder.typicode.com/todos/1"
+//     );
+
+//     const data = await response.json();
+
+//     console.log("TODO RESULT: ", data);
+//   } catch (error) {
+//     console.log("ERROR: ", error);
+//   }
+// }
+
+// getTodo();
+
+let myPromise1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("File1 has been converted successfully.");
+    // reject("Error while converting....");
+  }, 1000);
+});
+
+let myPromise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("File2 has been converted successfully.");
+    // reject("Error while converting....");
+  }, 1000);
+});
+
+let myPromise3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("File3 has been converted successfully.");
+    // reject("Error while converting....");
+  }, 1000);
+});
+
+// Promise.all([myPromise1, myPromise2, myPromise3])
+//   .then((data) => {
+//   console.log('DATA: ', data)
+// })
+//   .catch((error) => {
+// console.log("ERROR: ", error);
+// });
+
+async function getAllResults() {
   try {
-    const response = await fetch(
-      "https://jsonplaceholder.typicode.com/todos/1"
-    );
-
-    const data = await response.json();
-
-    console.log("TODO RESULT: ", data);
+    let results = await Promise.all([myPromise1, myPromise2, myPromise3]);
+    console.log("ALL RESULTS: ", results);
   } catch (error) {
     console.log("ERROR: ", error);
   }
 }
 
-getTodo();
+getAllResults();
